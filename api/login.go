cd0515600a73bbad930a9e857454e79c7724f436
dylan90302 <jdylan70@gmail.com>
2024-05-22 12:13:04 +0800

    complete the login session

--- api/login.go
+++ api/login.go
@@ -1,22 +1,18 @@
-package main
+package api
 
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/http/cookiejar"
 	"os"
 	"strings"
 )
 
-func login(username string, password string) {
-	jar, _ := cookiejar.New(nil)
-	client := http.Client{
-		Jar: jar,
-	}
+func Login(username string, password string, client http.Client) {
 	baseurl := "https://api.vrchat.cloud/api/1/auth/user"
 	req, _ := http.NewRequest("GET", baseurl, nil)
 	req.SetBasicAuth(username, password)
@@ -30,11 +26,22 @@ func login(username string, password string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", body)
-	if strings.Contains(string(body), "mail") {
+	fmt.Println(string(body))
+	type respjsn struct {
+		Resp []string `json:"requiresTwoFactorAuth"`
+	}
+	jsn := respjsn{}
+	err = json.Unmarshal(body, &jsn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if jsn.Resp == nil {
+		return
+	}
+	if jsn.Resp[0] == "emailOtp" {
 		emailapiurl := "https://api.vrchat.cloud/api/1/auth/twofactorauth/emailotp/verify"
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Email code Plz : \n")
+		fmt.Println("Email code Plz :")
 		code, _ := reader.ReadString('\n')
 		fmt.Println(code)
 		var codejsn = []byte(`{"code":"` + strings.TrimSpace(code) + `"}`)
@@ -46,12 +53,21 @@ func login(username string, password string) {
 		body, _ := io.ReadAll(resp.Body)
 		defer resp.Body.Close()
 		fmt.Printf("Respones : %s", body)
-	} else if strings.Contains(string(body), "app") {
+	} else if jsn.Resp[0] == "totp" {
+		fa2apiurl := "https://api.vrchat.cloud/api/1/auth/twofactorauth/totp/verify"
+		reader := bufio.NewReader(os.Stdin)
 		fmt.Printf("App code Plz : \n")
+		code, _ := reader.ReadString('\n')
+		fmt.Println(code)
+		var codejsn = []byte(`{"code":"` + strings.TrimSpace(code) + `"}`)
+		fmt.Println(string(codejsn))
+		reqa, _ := http.NewRequest("POST", fa2apiurl, bytes.NewBuffer(codejsn))
+		reqa.Header.Set("Content-Type", "application/json")
+		reqa.Header.Set("User-Agent", "VrcGo/0.1.0 jdylan70@gmail.com")
+		resp, _ := client.Do(reqa)
+		body, _ := io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		fmt.Printf("Respones : %s", body)
 	}
-	return
-}
-
-func main() {
-	login("violet404", "Amber7300.")
+	Login(username, password, client)
 }
--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-type model struct{}
+import (
+	api "Govrc/api"
+	"bufio"
+	"fmt"
+	"net/http"
+	"net/http/cookiejar"
+	"os"
+	"strings"
+)
 
-func (m model) Init() tea.Cmd{
-	return nil
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Username Plz :")
+	username, _ := reader.ReadString('\n')
+	reader1 := bufio.NewReader(os.Stdin)
+	fmt.Println("Password Plz :")
+	password, _ := reader1.ReadString('\n')
+
+	jar, _ := cookiejar.New(nil)
+	client := http.Client{
+		Jar: jar,
+	}
+	api.Login(strings.TrimSpace(username), strings.TrimSpace(password), client)
 }
-func (m model)
