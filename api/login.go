e32146121f7913fdc0108ef5a2842ecbe5f05c45
dylan90302 <jdylan70@gmail.com>
2024-05-22 02:37:17 +0000

    complete the login

--- api/login.go
+++ api/login.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	b64 "encoding/base64"
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"os"
+	"strings"
 )
 
-func encodelogin(username string, password string) string {
-	auth := username + ":" + password
-	return b64.StdEncoding.EncodeToString([]byte(auth))
-}
-
-func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
-	req.Header.Add("Authorization", "Basic "+encodelogin("username1", "password123"))
-	return nil
-}
-
-func main() {
+func login(username string, password string) {
+	jar, _ := cookiejar.New(nil)
+	client := http.Client{
+		Jar: jar,
+	}
 	baseurl := "https://api.vrchat.cloud/api/1/auth/user"
 	req, _ := http.NewRequest("GET", baseurl, nil)
-	req.SetBasicAuth("barrtya", "cyrus90302")
+	req.SetBasicAuth(username, password)
 	req.Header.Set("User-Agent", "VrcGo/0.1.0 jdylan70@gmail.com")
-	res, err := http.DefaultClient.Do(req)
+	res, err := client.Do(req)
 	body, _ := io.ReadAll(res.Body)
 	res.Body.Close()
 	if res.StatusCode > 200 {
@@ -32,5 +30,28 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", body)
+	fmt.Printf("%s\n", body)
+	if strings.Contains(string(body), "mail") {
+		emailapiurl := "https://api.vrchat.cloud/api/1/auth/twofactorauth/emailotp/verify"
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Email code Plz : \n")
+		code, _ := reader.ReadString('\n')
+		fmt.Println(code)
+		var codejsn = []byte(`{"code":"` + strings.TrimSpace(code) + `"}`)
+		fmt.Println(string(codejsn))
+		reqa, _ := http.NewRequest("POST", emailapiurl, bytes.NewBuffer(codejsn))
+		reqa.Header.Set("Content-Type", "application/json")
+		reqa.Header.Set("User-Agent", "VrcGo/0.1.0 jdylan70@gmail.com")
+		resp, _ := client.Do(reqa)
+		body, _ := io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		fmt.Printf("Respones : %s", body)
+	} else if strings.Contains(string(body), "app") {
+		fmt.Printf("App code Plz : \n")
+	}
+	return
+}
+
+func main() {
+	login("violet404", "Amber7300.")
 }
